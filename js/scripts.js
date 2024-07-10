9733f45ed18e141b6d9ea2cc5160a91027ccc454
UlrichArcadio <ulricof@gmail.com>
2024-07-10 13:23:29 +0200

    finished series

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+var myCarousel1 = document.querySelector('#carouselWorksControl1')
+var myModalEl1 = document.getElementById('worksModal1')
+
+myModalEl1.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(myCarousel1)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var myCarousel2 = document.querySelector('#carouselWorksControl2')
+var myModalEl2 = document.getElementById('worksModal2')
+
+myModalEl2.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(myCarousel2)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
 
 
 
--- js/scriptsSeries.js
+++ js/scriptsSeries.js
@@ -1,7 +1,159 @@
-var seriesCarousel = document.querySelector('#carouselSeriesControl')
-var seriesModalEl = document.getElementById('seriesModal')
-seriesModalEl.addEventListener('show.bs.modal', function (event) {
+var seriesCarousel1 = document.querySelector('#carouselSeriesControl1')
+var seriesModalEl1 = document.getElementById('seriesModal1')
+seriesModalEl1.addEventListener('show.bs.modal', function (event) {
     const trigger = event.relatedTarget
-    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel)
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel1)
     bsCarousel.to(trigger.dataset.bsSlideTo)
 })
+
+
+var seriesCarousel2 = document.querySelector('#carouselSeriesControl2')
+var seriesModalEl2 = document.getElementById('seriesModal2')
+seriesModalEl2.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel2)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel3 = document.querySelector('#carouselSeriesControl3')
+var seriesModalEl3 = document.getElementById('seriesModal3')
+seriesModalEl3.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel3)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel4 = document.querySelector('#carouselSeriesControl4')
+var seriesModalEl4 = document.getElementById('seriesModal4')
+seriesModalEl4.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel4)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel5 = document.querySelector('#carouselSeriesControl5')
+var seriesModalEl5 = document.getElementById('seriesModal5')
+seriesModalEl5.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel5)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+
+var seriesCarousel6 = document.querySelector('#carouselSeriesControl6')
+var seriesModalEl6 = document.getElementById('seriesModal6')
+seriesModalEl6.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel6)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel7 = document.querySelector('#carouselSeriesControl7')
+var seriesModalEl7 = document.getElementById('seriesModal7')
+seriesModalEl7.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel7)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel8 = document.querySelector('#carouselSeriesControl8')
+var seriesModalEl8 = document.getElementById('seriesModal8')
+seriesModalEl8.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel8)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel9 = document.querySelector('#carouselSeriesControl9')
+var seriesModalEl9 = document.getElementById('seriesModal9')
+seriesModalEl9.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel9)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+
+var seriesCarousel10 = document.querySelector('#carouselSeriesControl10')
+var seriesModalEl10 = document.getElementById('seriesModal10')
+seriesModalEl10.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel10)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+
+var seriesCarousel11 = document.querySelector('#carouselSeriesControl11')
+var seriesModalEl11 = document.getElementById('seriesModal11')
+seriesModalEl11.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel11)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+
+var seriesCarousel12 = document.querySelector('#carouselSeriesControl12')
+var seriesModalEl12 = document.getElementById('seriesModal12')
+seriesModalEl12.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel12)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel13 = document.querySelector('#carouselSeriesControl13')
+var seriesModalEl13 = document.getElementById('seriesModal13')
+seriesModalEl13.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel13)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel14 = document.querySelector('#carouselSeriesControl14')
+var seriesModalEl14 = document.getElementById('seriesModal14')
+seriesModalEl14.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel14)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel15 = document.querySelector('#carouselSeriesControl15')
+var seriesModalEl15 = document.getElementById('seriesModal15')
+seriesModalEl15.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel15)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+
+var seriesCarousel16 = document.querySelector('#carouselSeriesControl16')
+var seriesModalEl16 = document.getElementById('seriesModal16')
+seriesModalEl16.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel16)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel17 = document.querySelector('#carouselSeriesControl17')
+var seriesModalEl17 = document.getElementById('seriesModal17')
+seriesModalEl17.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel17)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel18 = document.querySelector('#carouselSeriesControl18')
+var seriesModalEl18 = document.getElementById('seriesModal18')
+seriesModalEl18.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel18)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+var seriesCarousel19 = document.querySelector('#carouselSeriesControl19')
+var seriesModalEl19 = document.getElementById('seriesModal19')
+seriesModalEl19.addEventListener('show.bs.modal', function (event) {
+    const trigger = event.relatedTarget
+    var bsCarousel = bootstrap.Carousel.getInstance(seriesCarousel19)
+    bsCarousel.to(trigger.dataset.bsSlideTo)
+})
+
+
