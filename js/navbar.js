bcfd10207d9e61446878a618f0285afc7904e9a0
UlrichArcadio <ulricof@gmail.com>
2025-06-23 15:19:20 +0200

    refactored navbar

---
+++ js/navbar.js
@@ -0,0 +1,39 @@
+// js/navbar.js
+// Fügt die Navbar dynamisch in das Element mit der ID 'navbar-container' ein
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarHTML = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <div class="container px-5">
+            <a class="navbar-brand" href="index.html">
+                Amanda Carmona Bosch
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                            <a class="nav-link me-lg-3" href="works.html">Obra</a>
+                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-lg-3" href="biography.html">Biografía</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-lg-3" href="publicaciones.html">Publicaciones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-lg-3" href="contact.html">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar  -->
+    `;
+    const container = document.getElementById('navbar-container');
+    if (container) {
+        container.innerHTML = navbarHTML;
+    }
+});
+
