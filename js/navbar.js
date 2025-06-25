79832d7e32bc58f4d1b2c38567ee9a0399c6aeef
UlrichArcadio <ulricof@gmail.com>
2025-06-25 12:59:50 +0200

    drop down menu on the right

--- js/navbar.js
+++ js/navbar.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                             <a class="nav-link me-lg-3" href="works.html">Obra</a>
@@ -36,4 +36,3 @@ document.addEventListener('DOMContentLoaded', function() {
         container.innerHTML = navbarHTML;
     }
 });
-
