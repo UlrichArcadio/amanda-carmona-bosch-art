5c81cf185e7031a82953679dabb2ea6d7d766588
UlrichArcadio <ulricof@gmail.com>
2025-06-25 15:02:32 +0200

    mejore la pagina works

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <div class="container px-5">
             <a class="navbar-brand" href="index.html">
-                Amanda Carmona Bosch
+                AMANDA CARMONA BOSCH
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -14,16 +14,16 @@ document.addEventListener('DOMContentLoaded', function() {
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                            <a class="nav-link me-lg-3" href="works.html">Obra</a>
+                            <a class="nav-link me-lg-3" href="works.html">OBRA</a>
                         </li>
                     <li class="nav-item">
-                        <a class="nav-link me-lg-3" href="biography.html">Biografía</a>
+                        <a class="nav-link me-lg-3" href="biography.html">BIOGRAFIA</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-lg-3" href="publicaciones.html">Publicaciones</a>
+                        <a class="nav-link me-lg-3" href="publicaciones.html">PUBLICACIONES</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-lg-3" href="contact.html">Contacto</a>
+                        <a class="nav-link me-lg-3" href="contact.html">CONTACTO</a>
                     </li>
                 </ul>
             </div>
